d69f8ea138d78c2405c02c3584b9e1d9f405483c
Masatomo Segawa <masa-segawa@masa-segawa.com>
2015-07-14 19:10:53 +0900

    バグをfix

--- KinokoTakenokoVS/Assets/Segawa/Scripts/TitleLogic.cs
+++ KinokoTakenokoVS/Assets/Segawa/Scripts/TitleLogic.cs
@@ -4,30 +4,23 @@ using System.Collections;
 
 public class TitleLogic : MonoBehaviour {
 
-    public bool endFlag = false;
-
     float coolTime = 1.0f;
     float nextTime;
 
     void Start()
     {
 
+		SoundManager.Instance.PlaySE("kasasu");
 
+		nextTime = Time.time + coolTime;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if ((Input.GetMouseButton(0) || Input.anyKey) && endFlag == true && nextTime <= Time.time)
+        if ((Input.GetMouseButton(0) || Input.anyKey) && nextTime <= Time.time)
         {
-            Application.LoadLevel("GamePlay");
-        }
-
-        if ((Input.GetMouseButton(0) || Input.anyKey) && endFlag == false)
-        {
-            this.GetComponent<Animator>().SetTrigger("Skip");
-            SoundManager.Instance.PlaySE("kasasu");
-            nextTime = Time.time + coolTime;
+			FadeManager.Instance.LoadLevel ("GamePlay", 1.0f);
         }
 
 	}
